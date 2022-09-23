9b4022b873149a6868c34e38a67c36fb399d5a42
benjaminsal92@gmail.com <benjaminsal92@gmail.com>
2022-09-23 11:07:38 +0100

    first commit

---
+++ src/main/java/com/example/codeclan/capstoneproject/Campagna/CampagnaApplication.java
@@ -0,0 +1,13 @@
+package com.example.codeclan.capstoneproject.Campagna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampagnaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampagnaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/codeclan/capstoneproject/Campagna/CampagnaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.codeclan.capstoneproject.Campagna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampagnaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
