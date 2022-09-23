c9b5c4455c2cb4fc5bf39a02431fde3c073e2391
peppino <peppinocasula@yahoo.it>
2022-09-23 11:29:40 +0100

    Added Rooms classes

---
+++ src/main/java/com/example/codeclan/capstoneproject/Campagna/Accomodation/Models/BAndBRoom.java
@@ -0,0 +1,8 @@
+package com.example.codeclan.capstoneproject.Campagna.Accomodation.Models;
+
+public class BAndBRoom extends Room{
+
+    public BAndBRoom(String name, int capacity, int price) {
+        super(name, capacity, price);
+    }
+}
