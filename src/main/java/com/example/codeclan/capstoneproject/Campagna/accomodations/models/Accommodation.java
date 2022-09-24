fc68560bbe16071e7e8cc0393608fb6de0f67c2f
benjaminsal92@gmail.com <benjaminsal92@gmail.com>
2022-09-24 17:23:49 +0100

    Need to restart, started on IBook interface

--- src/main/java/com/example/codeclan/capstoneproject/Campagna/accomodations/models/Accommodation.java
+++ src/main/java/com/example/codeclan/capstoneproject/Campagna/accomodations/models/Accommodation.java
@@ -1,14 +1,46 @@
 package com.example.codeclan.capstoneproject.Campagna.accomodations.models;
 
+import com.example.codeclan.capstoneproject.Campagna.accomodations.bookings.Booking;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
 public abstract class Accommodation {
 
     private final String name;
+    private List<Booking> bookings;
 
     public Accommodation(String name) {
         this.name = name;
+        this.bookings = new ArrayList<>();
     }
 
     public String getName() {
         return name;
     }
+
+    public List<Booking> getBookings() {
+        return bookings;
+    }
+
+    //not sure if these methods need to be here or in room
+    public void addBooking(Booking booking){
+        this.bookings.add(booking);
+    }
+
+    public Booking makeBooking(int day, int month, int year, int numberOfDays){
+        Booking booking = new Booking(day, month, year, numberOfDays);
+        return booking;
+    }
+
+    public boolean checkNotDoubleBooked(Booking possibleBooking){
+        boolean doubleBooked = false;
+        for(Booking booking : this.bookings){
+            doubleBooked = Stream.of(booking.getDaysBooked()).anyMatch(possibleBooking.getDaysBooked()::contains);
+        }
+        return doubleBooked;
+    }
+
+    //overall booking
 }
--- src/test/java/com/example/codeclan/capstoneproject/Campagna/accomodations/bookingsTest/BookingTest.java
+++ src/test/java/com/example/codeclan/capstoneproject/Campagna/accomodations/bookingsTest/BookingTest.java
@@ -18,10 +18,15 @@ public class BookingTest {
     LocalDate date1;
     LocalDate date3;
     LocalDate date4;
+    LocalDate afterNewYear;
     LocalDate newYear;
+    LocalDate newYearEve;
+    LocalDate newYearEveEve;
     LocalDate testNewYear;
     Booking booking;
+    Booking newYearBooking;
     List<LocalDate> daysBooked;
+    List<LocalDate> nearNewYearBooked;
     LocalDate dayBooked1;
     LocalDate dayBooked2;
     LocalDate dayBooked3;
@@ -33,6 +38,7 @@ public class BookingTest {
     @BeforeEach
     public void before(){
         booking = new Booking(2020, 1, 10, 5);
+        newYearBooking = new Booking(2020, 12, 30, 4);
         daysBooked = new ArrayList<>();
         dayBooked1 = LocalDate.of(2020, 1, 10);
         dayBooked2 = LocalDate.of(2020, 1, 11);
@@ -48,7 +54,15 @@ public class BookingTest {
         date1 = LocalDate.of(2022, 2, booking.gettingToTheEndOfMonth(29, 2));
         date3 = LocalDate.of(2020, 12, 12);
         date4 = LocalDate.of(2020, 12, 12);
+        newYearEveEve = LocalDate.of(2020, 12, 30);
+        newYearEve = LocalDate.of(2020, 12, 31);
         newYear = LocalDate.of(2021, 1, 1);
+        afterNewYear = LocalDate.of(2021, 1, 2);
+        nearNewYearBooked = new ArrayList<>();
+        nearNewYearBooked.add(newYearEveEve);
+        nearNewYearBooked.add(newYearEve);
+        nearNewYearBooked.add(newYear);
+        nearNewYearBooked.add(afterNewYear);
         testNewYear = booking.bookDate(2020, 12, 32);
 
         stringOne = new ArrayList<>();
@@ -87,5 +101,10 @@ public class BookingTest {
     @Test
     public void assertDeepEquals(){
         assertArrayEquals(new List[]{stringOne}, new List[]{stringTwo});
+    }
+
+    @Test
+    public void checkNearNewYearWorks(){
+        assertArrayEquals(new List[]{nearNewYearBooked}, new List[]{newYearBooking.getDaysBooked()});
     }
 }
