5161841fbdeb7eb0fe8050fd36fde00c533deac8
benjaminsal92@gmail.com <benjaminsal92@gmail.com>
2022-09-24 18:13:00 +0100

    house can campSite can both make booking properly Tested

--- src/main/java/com/example/codeclan/capstoneproject/Campagna/accomodations/models/CampSitePitch.java
+++ src/main/java/com/example/codeclan/capstoneproject/Campagna/accomodations/models/CampSitePitch.java
@@ -1,15 +1,24 @@
 package com.example.codeclan.capstoneproject.Campagna.accomodations.models;
 
+import com.example.codeclan.capstoneproject.Campagna.accomodations.bookings.Booking;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
 public class CampSitePitch{
 
     private String name;
     private int capacity;
     private int price;
+    private List<Booking> bookings;
 
     public CampSitePitch(String name, int capacity, int price){
         this.name = name;
         this.capacity = capacity;
         this.price = price;
+        this.bookings = new ArrayList<>();
     }
 
     public String getName() {
@@ -34,5 +43,43 @@ public class CampSitePitch{
 
     public void setPrice(int price) {
         this.price = price;
+    }
+
+    public List<Booking> getBookings() {
+        return bookings;
+    }
+
+    public List<LocalDate> getBookedDays(){
+        List<LocalDate> daysRoomIsBooked = new ArrayList<>();
+        for(Booking booking : this.bookings){
+            daysRoomIsBooked.addAll(booking.getDaysBooked());
+        }
+        return daysRoomIsBooked;
+    }
+
+    public void addBooking(Booking booking){
+        this.bookings.add(booking);
+    }
+
+    public Booking newBooking(int day, int month, int year, int numberOfDays){
+        Booking booking = new Booking(day, month, year, numberOfDays);
+        return booking;
+    }
+
+    public boolean checkNotDoubleBooked(Booking possibleBooking){
+        boolean doubleBooked = false;
+        for(Booking booking : this.bookings){
+            doubleBooked = Stream.of(booking.getDaysBooked()).anyMatch(possibleBooking.getDaysBooked()::contains);
+        }
+        return doubleBooked;
+    }
+
+    public void makeBooking(int day, int month, int year, int numberOfDays){
+        Booking possibleBooking = newBooking(day , month , year , numberOfDays );
+        if (checkNotDoubleBooked(possibleBooking)){
+            return;
+        } else {
+            addBooking(possibleBooking);
+        }
     }
 }
--- src/main/java/com/example/codeclan/capstoneproject/Campagna/accomodations/models/House.java
+++ src/main/java/com/example/codeclan/capstoneproject/Campagna/accomodations/models/House.java
@@ -1,14 +1,25 @@
 package com.example.codeclan.capstoneproject.Campagna.accomodations.models;
 
+import com.example.codeclan.capstoneproject.Campagna.accomodations.bookings.Booking;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
 public class House extends Accommodation{
 
     private int capacity;
+    private int price;
     private int currentCapacity;
+    private List<Booking> bookings;
 
-    public House(String name, int capacity) {
+    public House(String name, int capacity, int price) {
         super(name);
+        this.price = price;
         this.capacity = capacity;
         this.currentCapacity = this.capacity;
+        this.bookings = new ArrayList<>();
     }
 
     public int getCapacity() {
@@ -29,5 +40,43 @@ public class House extends Accommodation{
 
     public void emptyHouse(){
         this.currentCapacity = this.capacity;
+    }
+
+    public List<Booking> getBookings() {
+        return bookings;
+    }
+
+    public List<LocalDate> getBookedDays(){
+        List<LocalDate> daysRoomIsBooked = new ArrayList<>();
+        for(Booking booking : this.bookings){
+            daysRoomIsBooked.addAll(booking.getDaysBooked());
+        }
+        return daysRoomIsBooked;
+    }
+
+    public void addBooking(Booking booking){
+        this.bookings.add(booking);
+    }
+
+    public Booking newBooking(int day, int month, int year, int numberOfDays){
+        Booking booking = new Booking(day, month, year, numberOfDays);
+        return booking;
+    }
+
+    public boolean checkNotDoubleBooked(Booking possibleBooking){
+        boolean doubleBooked = false;
+        for(Booking booking : this.bookings){
+            doubleBooked = Stream.of(booking.getDaysBooked()).anyMatch(possibleBooking.getDaysBooked()::contains);
+        }
+        return doubleBooked;
+    }
+
+    public void makeBooking(int day, int month, int year, int numberOfDays){
+        Booking possibleBooking = newBooking(day , month , year , numberOfDays );
+        if (checkNotDoubleBooked(possibleBooking)){
+            return;
+        } else {
+            addBooking(possibleBooking);
+        }
     }
 }
